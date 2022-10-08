28f430394b501d87d26ebb716c054e7f453a5d31
Lavanya <104674261+Lavanyak03@users.noreply.github.com>
2022-10-08 15:39:33 +0530

    Add files via upload

---
+++ stop-sign-detection.py
@@ -0,0 +1,20 @@
+import cv2
+from matplotlib import pyplot as plt
+img=cv2.imread("stop-2.jpg")
+img_gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img_rgb=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+#for showing the picture
+plt.subplot(1,1,1)
+plt.imshow(img_rgb)
+plt.show()
+#to detect big and small images
+stop_data=cv2.CascadeClassifier("stop_data.xml")
+found=stop_data.detectMultiScale(img_gray,minSize =(20, 20))
+amount_found=len(found)
+if amount_found!=0:
+  for(x,y,width,height) in found:
+    cv2.rectangle(img_rgb,(x,y),(x+height,y+width),(0,255,0),5)
+#for showing the picture
+plt.subplot(1,1,1)
+plt.imshow(img_rgb)
+plt.show()
